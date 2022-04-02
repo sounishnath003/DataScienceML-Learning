e87e43640679cc400965af2cd1caac353f3a6b69
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2022-04-02 18:17:16 +0530

    Update cleanser.py

--- NLP_Tasks/cleanser.py
+++ NLP_Tasks/cleanser.py
@@ -3,9 +3,8 @@
 import gc
 import re
 
-import nltk
 from sklearn import feature_extraction
-
+import nltk
 
 class TextCleanser:
     def __init__(self, texts) -> None:
@@ -20,6 +19,7 @@ class TextCleanser:
             temptext = pattern.sub(r'', sentence)
             xclean.append(temptext)
 
+        self.texts = xclean
         return xclean
 
     def remove_urls(self):
@@ -30,6 +30,7 @@ class TextCleanser:
             temptext = pattern.sub(r'', sentence)
             xclean.append(temptext)
 
+        self.texts = xclean
         return xclean
 
     def remove_punctuations(self):
@@ -37,15 +38,15 @@ class TextCleanser:
         xclean = []
         excluders = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
         for sentence in self.texts:
-            temptext = sentence.translate(str.maketrans('', '', excluders))
+            temptext = sentence.translate( str.maketrans('', '', excluders) )
             xclean.append(temptext)
 
+        self.texts = xclean
         return xclean
 
     def remove_stop_words(self, custom_words=[]):
         gc.collect()
-        self.stop_words = feature_extraction.text.ENGLISH_STOP_WORDS.union(
-            custom_words)
+        self.stop_words = feature_extraction.text.ENGLISH_STOP_WORDS.union(custom_words)
         xclean = []
 
         for text in self.texts:
@@ -57,24 +58,26 @@ class TextCleanser:
             sentence = " ".join(words)
             xclean.append(sentence)
 
+        self.texts = xclean
         return xclean
 
     def remove_emoji_or_non_unicodes(self):
         gc.collect()
         emoji_pattern = re.compile("["
-                                   u"\U0001F600-\U0001F64F"  # emoticons
-                                   u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-                                   u"\U0001F680-\U0001F6FF"  # transport & map symbols
-                                   u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-                                   u"\U00002702-\U000027B0"
-                                   u"\U000024C2-\U0001F251"
-                                   "]+", flags=re.UNICODE)
+                                u"\U0001F600-\U0001F64F"  # emoticons
+                                u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                                u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                                u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                                u"\U00002702-\U000027B0"
+                                u"\U000024C2-\U0001F251"
+                                "]+", flags=re.UNICODE)
 
         xclean = []
         for text in self.texts:
             temptext = emoji_pattern.sub(r"", text)
             xclean.append(temptext)
-
+        
+        self.texts = xclean
         return xclean
 
     def perform_stem(self):
@@ -82,10 +85,10 @@ class TextCleanser:
         xclean = []
         ps = nltk.stem.PorterStemmer()
         for text in self.texts:
-            temptext = " ".join([ps.stem(word)
-                                for word in nltk.word_tokenize(text)])
+            temptext = " ".join([ps.stem(word) for word in nltk.word_tokenize(text)])
             xclean.append(temptext)
-
+        
+        self.texts = xclean
         return xclean
 
     def perform_lemmatize(self):
@@ -97,4 +100,5 @@ class TextCleanser:
                                 for word in nltk.word_tokenize(text)])
             xclean.append(temptext)
 
+        self.texts = xclean
         return xclean

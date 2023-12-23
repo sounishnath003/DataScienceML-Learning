ac3002d71cddc7ce621480e97877a8dfbc7565ab
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2023-12-23 19:39:16 +0530

    adding @desc to project-generator-v2

--- pyproject-generator/pyproject-generator/project_generator.py
+++ pyproject-generator/pyproject-generator/project_generator.py
@@ -1,3 +1,16 @@
+"""
+# _* coding: utf8 *_
+
+filename: project_generator.py
+
+@author: sounishnath003
+@description: A custom machine learning project scaffolding generator script to reduce inventing the wheel. Inspired by @Angular project skafolder. Extensible, maintainable and easy configurable setup. 
+
+usage: python3 -m project_generator --projectName=sentiment-analyzer-project
+
+createdAt: 2023-07-26 09:55:32
+"""
+
 import os
 import logging
 import argparse
@@ -8,7 +21,7 @@ def run():
     logging.getLogger().setLevel(logging.INFO)
     logging.basicConfig(level=logging.INFO, format="[%(asctime)s]: %(message)s")
 
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(prog="ml-project-generator")
     parser.add_argument("--projectName", type=str, required=True)
     opts, pipeline_opts = parser.parse_known_args()
 
@@ -19,7 +32,7 @@ def run():
     logging.info("===================================")
 
     project_files_to_be_provisioned = [
-        "artifacts/store",
+        "artifacts/kubeflow_store/store",
         "setup.py",
         "test.py",
         "logs/activity.log",
@@ -33,9 +46,12 @@ def run():
         "trainer/lightning.py",
         "main.py",
         "inference.py",
+        "torchserve/custom_inference_handler.py",
         "config/__init__.py",
-        "config/config.py",
+        "config/configuration.py",
         "notebooks/research.ipynb",
+        "inference_service/__init__.py",
+        "inference_service/handlers.py",
         "inference_service/server.py",
         "kubeflow/components/__init__.py",
         "kubeflow/create_pipeline.py",
